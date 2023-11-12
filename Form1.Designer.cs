7ee9b84d40f0809cfb716a36b68e161365e28000
0523ronli <0523ronli@gmail.com>
2023-11-12 10:45:23 +0800

    add a dumb sizer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // textBox1
@@ -41,15 +42,28 @@
             textBox1.Size = new Size(223, 450);
             textBox1.TabIndex = 0;
             // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(759, 431);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 19);
+            label1.TabIndex = 1;
+            label1.Text = "sizer";
+            label1.MouseDown += label1_MouseDown;
+            // 
             // Form1
             // 
             AllowDrop = true;
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(textBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
+            TopMost = true;
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -58,5 +72,6 @@
         #endregion
 
         private TextBox textBox1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,8 @@
 //using System.Windows.Forms;
 
+using System.Net.Mail;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 namespace Bachup_s_backup
 {
     public partial class Form1 : Form
@@ -8,15 +11,20 @@ namespace Bachup_s_backup
         public Form1()
         {
             InitializeComponent();
+
             TopMost = true;
-            this.MinimizeBox = false;
+            //this.MinimizeBox = false;
+            FormBorderStyle = FormBorderStyle.Sizable;
             textBox1.DragEnter += MyDragEnter;
             textBox1.DragDrop += textBox1_DragDrop;
             MouseDown += (s, e) =>
             {
-                Capture=false;
-                Message msg = Message.Create(Handle, 0xA1, 0x2, 0x0);
+                Capture = false;
+                var p = new Point(0, 0);
+                Message msg = Message.Create(Handle, 161, 0x2, ((p.Y << 16) | p.X));
+                msg = Message.Create(Handle, 0x112 /*WM_SYSCOMMAND*/, 0xF008 /*SC_SIZE + WMSZ_BOTTOMRIGHT*/, 0);
                 WndProc(ref msg);
+
             };
             DragEnter += (s, e) =>
             {
@@ -66,6 +74,31 @@ namespace Bachup_s_backup
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        protected override void WndProc(ref Message m)
+        {
+
+            base.WndProc(ref m);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Point lParam = Cursor.Position;
+            //lParam = PointToClient(lParam);
+            //Message msg = Message.Create(Handle, 0x84 /*WM_NCHITTEST*/, IntPtr.Zero, (IntPtr)((lParam.Y << 16) | lParam.X));
+            //if ((int)msg.Result == 0x0 /*HTCLIENT*/)
+            //{
+            //    msg = Message.Create(Handle, 0x112 /*WM_SYSCOMMAND*/, (IntPtr)0xF008 /*SC_SIZE + WMSZ_BOTTOMRIGHT*/, IntPtr.Zero);
+            //}
+            label1.Capture = false;
+            var p = new Point(0, 0);
+            Message msg = Message.Create(Handle, 0x112 /*WM_SYSCOMMAND*/, 0xF008 /*SC_SIZE + WMSZ_BOTTOMRIGHT*/, 0);
+            WndProc(ref msg);
         }
     }
 }
