2a6f5606c3290bcdc2846f842b0e04eaa628e2e5
侑介 <spark20060609@gmail.com>
2023-12-06 10:24:42 +0800

    add forecolor

--- Config_JSON.cs
+++ Config_JSON.cs
@@ -12,9 +12,9 @@ namespace Bachup_s_backup
         public Point location { get; set; } = new(0, 0);
         public Size size { get; set; } = new(200, 450);
         public double Opacity { get; set; } = .700;
-        //public Color DI_default { get; set; } = SystemColors.ActiveBorder;
         public Color DI_selected { get; set; } = Color.LightBlue;
         public Color DI_backColor { get; set; } = SystemColors.ActiveBorder;
+        public Color DI_ForeColor { get; set; } = Color.Black;
         public Size DI_size { get; set; } = new Size(140, 140);
         public List<DI_Json> DI_List { get; set; } = new();
     }
